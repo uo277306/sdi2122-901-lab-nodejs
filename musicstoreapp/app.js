6c7080cc30154ad2eb5088bf36d7d1c77f292fc5
uo277306 <uo277306@uniovi.es>
2022-04-03 22:27:18 +0200

    SDI-2122-901-8.2-Arquitectura para el acceso a datos.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -18,7 +18,10 @@ const {MongoClient} = require("mongodb");
 const url = 'mongodb+srv://uo277306:Chocolatina.@tiendamusica.cu9is.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 app.set('connectionStrings', url);
 
-require("./routes/songs.js")(app, MongoClient);
+let songsRepository = require("./repositories/songsRepository.js");
+songsRepository.init(app, MongoClient);
+
+require("./routes/songs.js")(app, songsRepository);
 require("./routes/authors.js")(app);
 
 // view engine setup
--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,4 +1,4 @@
-module.exports = function (app, MongoClient) {
+module.exports = function (app, songsRepository) {
     app.get("/songs", function (req, res) {
         let songs = [
             {"title": "Blank space", "price": "1.2"},
@@ -19,19 +19,13 @@ module.exports = function (app, MongoClient) {
             title: req.body.title,
             kind: req.body.kind,
             price: req.body.price
-        }
+        };
 
-        MongoClient.connect(app.get('connectionStrings'), function (err, dbClient) {
-            if (err) {
-                res.send("Error de conexión: " + err);
+        songsRepository.insertSong(song, function (songId) {
+            if (songId == null) {
+                res.send("Error al insertar canción");
             } else {
-                const database = dbClient.db("musicStore");
-                const collectionName = 'songs';
-                const songsCollection = database.collection(collectionName);
-                songsCollection.insertOne(song)
-                    .then(result => res.send("canción añadida id: " + result.insertedId))
-                    .then(() => dbClient.close())
-                    .catch(err => res.send("Error al insertar " + err));
+                res.send("Agregada la canción con ID: " + songId)
             }
         });
     });
